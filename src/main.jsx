4ddab13976bc2c98ba02494625130fc5ca02139c
Nafis <bhuiyannafis517@gmail.com>
2024-01-16 16:30:22 +0600

    footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from './App.jsx'
 import './index.css'
+import Footer from './Footer';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Footer></Footer>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
