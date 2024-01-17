83bed69538629dec261fc734f30a258a70f43018
deltatranslator <deltatranslator16@gmail.com>
2024-01-17 14:06:15 +0600

    merge problem

--- src/components/TranslatorComponent/TranslatorComponent.jsx
+++ src/components/TranslatorComponent/TranslatorComponent.jsx
@@ -3,15 +3,15 @@ import SwipeArrow from "./SwipeArrow";
 import TargetLangComponent from "./TargetLangComponent";
 
 const TranslatorComponent = () => {
-    return (
-        <div>
-            <div className="container mx-auto flex flex-col lg:flex-row justify-center p-4 gap-4 my-12">
-                <SourceLangComponent />
-                <SwipeArrow />
-                <TargetLangComponent />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="container mx-auto flex flex-col lg:flex-row justify-center p-4 gap-4 my-12">
+        <SourceLangComponent />
+        <SwipeArrow />
+        <TargetLangComponent />
+      </div>
+    </div>
+  );
 };
 
 export default TranslatorComponent;
