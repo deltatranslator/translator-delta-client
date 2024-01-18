5426f6c4be9c69098d47ff9bad58e7ede29867e5
mahedi205339 <mahedi205339gmaol.com>
2024-01-18 18:45:36 +0600

    [feature](login & sign in) updated login and sign in design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,114 +1,79 @@
-import "../SignUp/signUp.css";
+/* eslint-disable react/no-unescaped-entities */
+// import "../SignUp/signUp.css";
 import { FcGoogle } from "react-icons/fc";
 import { useForm } from "react-hook-form";
+import Lottie from "lottie-react";
+import loginAnime from '../../assets/Animation - 1705578701251.json'
+import { Link } from "react-router-dom";
 const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    const email = data.email;
-    const password = data.password;
-    console.log(email, password);
-    reset();
-  };
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        const email = data.email;
+        const password = data.password;
+        console.log(email, password);
+        reset();
+    };
 
-  return (
-    <div className="signUp">
-      <div className="hero min-h-screen ">
-        <div className="hero-content flex-col lg:flex-row">
-          <div className="text-center lg:text-left text-yellow-500 ">
-            <h1 className="text-5xl font-bold ">Welcome Back!</h1>
-            <p className="py-6 text-white w-11/12">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card shrink-0 w-full max-w-sm lg:max-w-lg">
-            <h1 className="text-5xl font-bold text-yellow-600 ml-10">Login</h1>
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-yellow-500 font-semibold">
-                    Email Address
-                  </span>
-                </label>
-                <input
-                  {...register("email", { required: true })}
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered border-yellow-600  "
-                  required
-                />
-                {errors.name && (
-                  <span className="text-red-700 font-bold">
-                    Email is required
-                  </span>
-                )}
-              </div>
+    return (
+        <div className="sign-back hero min-h-screen ">
+            <div className="hero-content flex flex-col md:flex-row-reverse w-full lg:gap-10">
+                <div className="text-center md:w-full lg:text-left max-w-lg">
+                    <Lottie animationData={loginAnime}></Lottie>
+                </div>
+                <div className="card flex-shrink-0 w-96">
+                    <div className='text-left ml-10 text-[#ed7966] text-2xl md:text-4xl font-bold'>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-yellow-500 font-semibold">
-                    Password
-                  </span>
-                </label>
-                <input
-                  {...register("password", {
-                    required: true,
-                    minLength: 8,
-                    maxLength: 20,
-                    pattern: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/,
-                  })}
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  className="input input-bordered border-yellow-600 "
-                />
-                {errors.name && (
-                  <span className="text-red-700 font-bold">
-                    Password is required
-                  </span>
-                )}
-                {errors.password?.type === "minLength" && (
-                  <p className="text-red-600"> Password must be 8 character</p>
-                )}
-                {errors.password?.type === "maxLength" && (
-                  <p className="text-red-600">
-                    {" "}
-                    Password must be less than 20 character
-                  </p>
-                )}
-                {errors.password?.type === "pattern" && (
-                  <p className="text-red-600">
-                    {" "}
-                    Password must have one uppercase , one lowercase and one
-                    number
-                  </p>
-                )}
-              </div>
+                        Log In
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-[#ed7966] font-semibold">Email Address</span>
+                            </label>
+                            <input  {...register("email", { required: true })} type="email" placeholder="email" className="input input-bordered border-[#ed7966]  " required />
+                            {errors.name && <span className='text-red-700 font-bold'>Email is required</span>}
+                        </div>
 
-              <div className="form-control mt-6">
-                <button
-                  type="submit"
-                  className="btn bg-yellow-600 hover:bg-yellow-800 text-white font-semibold"
-                >
-                  Sign UP
-                </button>
-              </div>
-            </form>
-            <button className="btn border border-yellow-600 text-yellow-600 my-4 btn-outline w-56 ml-10">
-              <FcGoogle className="text-3xl"></FcGoogle>
-              Google
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-[#ed7966] font-semibold">Password</span>
+                            </label>
+                            <input  {...register("password", {
+                                required: true,
+                                minLength: 8,
+                                maxLength: 20,
+                                pattern: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/
+                            })} type="password" name="password" placeholder="password" className="input input-bordered border-[#ed7966] " />
+                            {errors.name && <span className='text-red-700 font-bold'>Password is required</span>}
+                            {errors.password?.type === 'minLength' && <p className='text-red-600'> Password must be 8 character</p>
+                            }
+                            {errors.password?.type === 'maxLength' && <p className='text-red-600'> Password must be less than 20 character</p>
+                            }
+                            {errors.password?.type === 'pattern' && <p className='text-red-600'> Password must have one uppercase , one lowercase and one number</p>
+                            }
+                        </div>
+
+                        <div className="form-control mt-6">
+                            <button type="submit" className="btn bg-[#ed7966] hover:bg-yellow-800 text-white font-semibold">Sign UP</button>
+                        </div>
+                    </form>
+
+                   
+                    <p className='text-center'><small className='text-[#ed7966]'>Don't Have an account? <Link to="/signup"><span className='font-bold'>Sign UP</span></Link></small></p>
+                    {/* social login  */}
+                    <button className="btn border border-yellow-600 text-yellow-600 my-4 btn-outline w-56 ml-10">
+                            <FcGoogle className="text-3xl"></FcGoogle>
+                            Google
+                        </button>
+                </div>
+            </div >
+        </div >
+    );
 };
 
 export default Login;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import "./signUp.css";
 import { GoUpload } from "react-icons/go";
 import { FcGoogle } from "react-icons/fc";
 import { useForm } from "react-hook-form";
+import Lottie from "lottie-react";
+import loginAnime from '../../assets/Animation - 1705578701251.json'
+import { Link } from "react-router-dom";
 const SignUp = () => {
   const {
     register,
@@ -18,139 +22,84 @@ const SignUp = () => {
   };
 
   return (
-    <div className="signUp">
-      <div className="hero min-h-screen ">
-        <div className="hero-content flex-col lg:flex-row">
-          <div className="text-center lg:text-left text-yellow-500 ">
-            <h1 className="text-5xl font-bold ">Welcome Back!</h1>
-            <p className="py-6 text-white w-11/12">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card shrink-0 w-full max-w-sm lg:max-w-lg">
-            <h1 className="text-5xl font-bold text-yellow-600 ml-10">
-              Sign UP
-            </h1>
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-yellow-500 font-semibold">
-                    Name
-                  </span>
-                </label>
-                <input
-                  {...register("name", { required: true })}
-                  type="text"
-                  placeholder="email"
-                  className="input input-bordered border-yellow-600  "
-                  required
-                />
-              </div>
-              {errors.name && (
-                <span className="text-red-700 font-bold">Name is required</span>
-              )}
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-yellow-500 font-semibold">
-                    Email Address
-                  </span>
-                </label>
-                <input
-                  {...register("email", { required: true })}
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered border-yellow-600  "
-                  required
-                />
-                {errors.name && (
-                  <span className="text-red-700 font-bold">
-                    Email is required
-                  </span>
-                )}
-              </div>
-
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-yellow-500 font-semibold">
-                    Password
-                  </span>
-                </label>
-                <input
-                  {...register("password", {
-                    required: true,
-                    minLength: 8,
-                    maxLength: 20,
-                    pattern: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/,
-                  })}
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  className="input input-bordered border-yellow-600 "
-                />
-                {errors.name && (
-                  <span className="text-red-700 font-bold">
-                    Password is required
-                  </span>
-                )}
-                {errors.password?.type === "minLength" && (
-                  <p className="text-red-600"> Password must be 8 character</p>
-                )}
-                {errors.password?.type === "maxLength" && (
-                  <p className="text-red-600">
-                    {" "}
-                    Password must be less than 20 character
-                  </p>
-                )}
-                {errors.password?.type === "pattern" && (
-                  <p className="text-red-600">
-                    {" "}
-                    Password must have one uppercase , one lowercase and one
-                    number
-                  </p>
-                )}
-              </div>
-              <div className="mt-7">
-                <label
-                  htmlFor="image"
-                  className="file-label  mb-2 text-sm text-white rounded-xl "
-                >
-                  <div className="flex justify-center items-center mx-auto">
-                    <GoUpload className="text-2xl font-bold" />
-                    <p>Upload Profile</p>
-                  </div>
-                </label>
-                <input
-                  {...register("image", { required: true })}
-                  type="file"
-                  id="image"
-                  name="image"
-                  accept="image/*"
-                />
-              </div>
-              {errors.image && (
-                <span className="text-red-700 font-bold">
-                  image is required
-                </span>
-              )}
-              <div className="form-control mt-6">
-                <button
-                  type="submit"
-                  className="btn bg-yellow-600 hover:bg-yellow-800 text-white font-semibold"
-                >
-                  Sign UP
-                </button>
-              </div>
-            </form>
-            <button className="btn border border-yellow-600 text-yellow-600 my-4 btn-outline w-56 ml-10">
-              <FcGoogle className="text-3xl"></FcGoogle>
-              Google
-            </button>
-          </div>
+    <div className="sign-back hero min-h-screen ">
+      <div className="hero-content flex flex-col md:flex-row-reverse w-full lg:gap-10">
+        <div className="text-center md:w-full lg:text-left max-w-lg">
+          <Lottie animationData={loginAnime}></Lottie>
         </div>
-      </div>
-    </div>
+        <div className="card flex-shrink-0 w-96">
+          <div className='text-left ml-10 text-[#ed7966] text-2xl md:text-4xl font-bold'>
+
+            Log In
+          </div>
+          {/* form */}
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#ed7966] font-semibold">Name</span>
+              </label>
+              <input {...register("name", { required: true })} type="text" placeholder="email" className="input input-bordered border-[#ed7966]  " required />
+            </div>
+            {errors.name && <span className='text-red-700 font-bold'>Name is required</span>}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#ed7966] font-semibold">Email Address</span>
+              </label>
+              <input  {...register("email", { required: true })} type="email" placeholder="email" className="input input-bordered border-[#ed7966]  " required />
+              {errors.name && <span className='text-red-700 font-bold'>Email is required</span>}
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#ed7966] font-semibold">Password</span>
+              </label>
+              <input  {...register("password", {
+                required: true,
+                minLength: 8,
+                maxLength: 20,
+                pattern: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/
+              })} type="password" name="password" placeholder="password" className="input input-bordered border-[#ed7966] " />
+              {errors.name && <span className='text-red-700 font-bold'>Password is required</span>}
+              {errors.password?.type === 'minLength' && <p className='text-red-600'> Password must be 8 character</p>
+              }
+              {errors.password?.type === 'maxLength' && <p className='text-red-600'> Password must be less than 20 character</p>
+              }
+              {errors.password?.type === 'pattern' && <p className='text-red-600'> Password must have one uppercase , one lowercase and one number</p>
+              }
+            </div>
+            <div className='mt-7'>
+              <label htmlFor='image' className='file-label  mb-2 text-sm text-white rounded-xl '>
+                <div className="flex justify-center items-center mx-auto">
+                  <GoUpload className="text-2xl font-bold" />
+                  <p>Upload Profile</p>
+                </div>
+
+              </label>
+              <input
+                {...register("image", { required: true })}
+                type='file'
+                id='image'
+                name='image'
+                accept='image/*'
+              />
+
+            </div>
+            {errors.image && <span className='text-red-700 font-bold'>image is required</span>}
+            <div className="form-control mt-6">
+              <button type="submit" className="btn bg-[#ed7966] hover:bg-yellow-800 text-white font-semibold">Sign UP</button>
+            </div>
+          </form>
+
+
+          <p className='text-center'><small className='text-[#ed7966]'>Don't Have an account? <Link to="/login"><span className='font-bold'>Login</span></Link></small></p>
+          {/* social login  */}
+          <button className="btn border border-[#ed7966] text-[#ed7966] my-4 btn-outline w-56 ml-10">
+            <FcGoogle className="text-3xl"></FcGoogle>
+            Google
+          </button>
+        </div>
+      </div >
+    </div >
   );
 };
 
