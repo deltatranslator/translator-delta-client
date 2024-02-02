720f24bc5281abd02741bb8048185bcfc7e8e7ff
deltatranslator <deltatranslator16@gmail.com>
2024-02-02 15:40:57 +0600

    from mehedi

--- src/components/PdfScan/PdfScan.jsx
+++ src/components/PdfScan/PdfScan.jsx
@@ -4,15 +4,14 @@ import TranslateSectionNav from "../TranslateSectionNav/TranslateSectionNav";
 import PdfScanArea from "./PdfScanArea";
 
 const PdfScan = () => {
-    return (
-        <div className="min-h-screen">
-            <Navbar/>
-            <TranslateSectionNav/>
-            <PdfScanArea/>
-            <Footer/>
-
-        </div>
-    );
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <TranslateSectionNav />
+      <PdfScanArea />
+      <Footer />
+    </div>
+  );
 };
 
 export default PdfScan;
