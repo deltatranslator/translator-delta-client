db20210d809526077c6222e6d84969f5963412bf
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-02-14 18:08:26 +0600

    updated a user profile dashboard section

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,88 +1,91 @@
-import { createContext, useEffect, useState } from 'react';
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-import { app } from '../Firebase/firebase.config';
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
 
-
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
-// google login Provider instance 
+// google login Provider instance
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  // onAuthStateChange
 
-    // onAuthStateChange 
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      // console.log('logged User ->', currentUser);
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
 
-    useEffect(() => {
-        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser)
-            setLoading(false)
-            // console.log('logged User ->', currentUser);
-        })
-        return () => {
-            return unSubscribe()
-        }
-    }, [])
+  // Create user or Registration user
 
-    // Create user or Registration user 
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const createUser = (email, password) => {
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  // user profile update
 
-    // user profile update 
+  const userProfileUpdate = (name, photo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
 
-    const userProfileUpdate = (name, photo) => {
-        setLoading(true)
-        return updateProfile(auth.currentUser, {
-            displayName: name,
-            photoURL: photo
-        })
+  // Google Login
 
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
 
-    }
+  // User login
 
-    // Google Login
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const googleLogin = () => {
-        setLoading(true)
-        return signInWithPopup(auth, provider)
-    }
+  // User logOut
 
-    // User login
+  const userLogOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    const userLogin = (email, password) => {
-        setLoading(true)
-        return signInWithEmailAndPassword(auth, email, password)
-    }
-
-    // User logOut 
-
-    const userLogOut = () => {
-        setLoading(true)
-        return signOut(auth)
-    }
-
-    // Auth Information
-    const authInfo = {
-        user,
-        loading,
-        createUser,
-        userProfileUpdate,
-        googleLogin,
-        userLogin,
-        userLogOut
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // Auth Information
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    userProfileUpdate,
+    googleLogin,
+    userLogin,
+    userLogOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/components/UserDashboard/UserProfile/UserProfile.jsx
+++ src/components/UserDashboard/UserProfile/UserProfile.jsx
@@ -1,36 +1,102 @@
 import Lottie from "lottie-react";
 import userAnimation from "../../../assets/userProfile.json";
 import useUser from "../../../hooks/useUser";
+import { FaChevronRight } from "react-icons/fa";
+import useAuth from "../../../hooks/useAuth";
 
 const UserProfile = () => {
   const [isUser] = useUser();
-  //   console.log(isUser);
+  const { user } = useAuth();
+  console.log(user);
 
   return (
     <>
-      <section className=" text-center mt-5 space-y-2">
-        <h3 className=" text-4xl font-medium text-slate-50">Personal info</h3>
-        <p className=" font-medium text-slate-300">
-          Info about you and your preferences across Delta services
-        </p>
-      </section>
-      <section className="md:flex flex-grow-0 items-center px-5">
-        <div className="space-y-2 flex-1">
-          <h2 className="text-2xl font-medium text-slate-50">
-            Your profile info in Delta services
-          </h2>
-          <p className=" text-slate-200">
-            Personal info and options to manage it. You can make some of this
-            info, like your contact details, visible to others so they can reach
-            you easily. You can also see a summary of your profiles.
+      <section className=" py-20">
+        {/* Personal Info */}
+        <section className=" text-center space-y-2">
+          <h3 className=" text-4xl font-medium text-slate-50">Personal info</h3>
+          <p className=" font-medium text-slate-300">
+            Info about you and your preferences across Delta services
           </p>
-        </div>
-        <div className="overflow-hidden">
-          <Lottie className="md:w-96 w-full" animationData={userAnimation} />
-        </div>
-      </section>
-      <section>
-        
+        </section>
+        <section className="md:flex flex-grow-0 items-center ml-10">
+          <div className="space-y-2 flex-1">
+            <h2 className="text-2xl font-medium text-slate-50">
+              Your profile info in Delta services
+            </h2>
+            <p className=" text-slate-200 w-10/12">
+              Personal info and options to manage it. You can make some of this
+              info, like your contact details, visible to others so they can
+              reach you easily. You can also see a summary of your profiles.
+            </p>
+          </div>
+          <div className="overflow-hidden">
+            <Lottie className="w-full md:w-80" animationData={userAnimation} />
+          </div>
+        </section>
+        <section className=" rounded-md border p-5 ml-10">
+          <div className=" md:flex flex-grow-0 items-center justify-between">
+            <div className=" space-y-12">
+              <div>
+                <h3 className=" text-2xl text-slate-50 font-medium">
+                  Basic info
+                </h3>
+                <p className=" text-slate-50 text-sm">
+                  Some info may be visible to other people using Google
+                  services.
+                </p>
+              </div>
+              <div className=" text-sm flex items-center gap-40 text-slate-50">
+                <p>Profile Picture</p>
+                <p>A profile picture helps personaliZe your account</p>
+              </div>
+            </div>
+            <div>
+              <div className="avatar">
+                <div className="w-40 mask mask-squircle">
+                  <img
+                    src={(isUser && isUser?.photo) || user?.photoURL}
+                    alt="Profile Picture"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <hr className="mt-5 mb-5" />
+            <div className=" flex items-center justify-between text-slate-50">
+              <div className=" flex items-center text-sm gap-[213px]">
+                <p>Name</p>
+                <p>
+                  {isUser && isUser.name
+                    ? isUser.name
+                    : user && user.displayName
+                    ? user.displayName
+                    : "Name Not Available"}
+                </p>
+              </div>
+              <div>
+                <FaChevronRight className=" text-2xl" />
+              </div>
+            </div>
+            <hr className="mt-5 mb-5" />
+            <div className=" flex items-center justify-between text-slate-50">
+              <div className=" flex items-center text-sm gap-[213px]">
+                <p>Email</p>
+                <p>
+                  {isUser && isUser.email
+                    ? isUser.email
+                    : user && user.email
+                    ? user.email
+                    : "Email Not Available"}
+                </p>
+              </div>
+              <div>
+                <FaChevronRight className=" text-2xl" />
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
     </>
   );
