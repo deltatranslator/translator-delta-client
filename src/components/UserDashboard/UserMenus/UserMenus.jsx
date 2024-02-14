edcec613e447da2faa0237abdf18e16d31ce3d98
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-02-14 13:41:12 +0600

    create a user profile route and implement a enemation

--- src/components/UserDashboard/UserMenus/UserMenus.jsx
+++ src/components/UserDashboard/UserMenus/UserMenus.jsx
@@ -5,9 +5,7 @@ import { HiMenuAlt3 } from "react-icons/hi";
 const UserMenus = () => {
   const [open, setOpen] = useState(true);
 
-  const isAdmin = true;
-
-  const adminMenus = [
+  const userMenus = [
     {
       name: "Profile",
       link: "/dashboard/userProfile",
@@ -42,39 +40,34 @@ const UserMenus = () => {
           ></HiMenuAlt3>
         </div>
         <div className="mt-4 flex flex-col gap-4 relative">
-          {isAdmin &&
-            adminMenus?.map((menu, i) => (
-              <Link
-                className={`${
-                  menu?.margin ? "mt-5" : menu?.marginBot ? "mb-5" : ""
-                } group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-[#eec38e] rounded-md`}
-                to={menu?.link}
-                key={i}
+          {userMenus?.map((menu, i) => (
+            <Link
+              className={`${
+                menu?.margin ? "mt-5" : menu?.marginBot ? "mb-5" : ""
+              } group flex items-center text-sm gap-3.5 font-medium p-2 hover:bg-[#eec38e] rounded-md`}
+              to={menu?.link}
+              key={i}
+            >
+              <div>{React.createElement(menu?.icon, { size: "24" })}</div>
+              <h2
+                style={{
+                  transitionDelay: `${i + 3}00ms`,
+                }}
+                className={`whitespace-pre duration-500 ${
+                  !open && "opacity-0 translate-x-28 overflow-hidden"
+                }`}
               >
-                <div>{React.createElement(menu?.icon, { size: "24" })}</div>
-                <h2
-                  style={{
-                    transitionDelay: `${i + 3}00ms`,
-                  }}
-                  className={`whitespace-pre duration-500 ${
-                    !open && "opacity-0 translate-x-28 overflow-hidden"
-                  }`}
-                >
-                  {menu?.name}
-                </h2>
-                <h2
-                  className={`${
-                    open && "hidden"
-                  } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}
-                >
-                  {menu?.name}
-                </h2>
-              </Link>
-            ))}
-        </div>
-        {/* User dashboard */}
-        <div>
-            <UserMenus />
+                {menu?.name}
+              </h2>
+              <h2
+                className={`${
+                  open && "hidden"
+                } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit`}
+              >
+                {menu?.name}
+              </h2>
+            </Link>
+          ))}
         </div>
       </div>
     </section>
--- src/components/UserDashboard/UserProfile/UserProfile.jsx
+++ src/components/UserDashboard/UserProfile/UserProfile.jsx
@@ -1,11 +1,31 @@
-
-
+import Lottie from "lottie-react";
+import userAnimation from "../../../assets/userProfile.json";
 const UserProfile = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <section className=" text-center mt-5 space-y-2">
+        <h3 className=" text-4xl font-medium text-slate-50">Personal info</h3>
+        <p className=" font-medium text-slate-300">
+          Info about you and your preferences across Delta services
+        </p>
+      </section>
+      <section className="md:flex flex-grow-0 items-center justify-evenly px-5">
+        <div className="space-y-2 flex-1">
+          <h2 className="text-2xl font-medium text-slate-50">
+            Your profile info in Google services
+          </h2>
+          <p className=" text-slate-200">
+            Personal info and options to manage it. You can make some of this
+            info, like your contact details, visible to others so they can reach
+            you easily. You can also see a summary of your profiles.
+          </p>
         </div>
-    );
+        <div className="overflow-hidden">
+          <Lottie className="md:w-96 w-full" animationData={userAnimation} />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default UserProfile;
