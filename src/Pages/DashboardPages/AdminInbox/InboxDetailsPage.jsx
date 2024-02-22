3276e09fd614c44435b4a2268776f1141850df30
deltatranslator <deltatranslator16@gmail.com>
2024-02-22 15:20:30 +0600

    subject added on contact section

--- src/Pages/DashboardPages/AdminInbox/InboxDetailsPage.jsx
+++ src/Pages/DashboardPages/AdminInbox/InboxDetailsPage.jsx
@@ -1,17 +1,33 @@
 import { useLoaderData } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import { FaUserCircle } from "react-icons/fa";
 
 const InboxDetailsPage = () => {
   const loadDetails = useLoaderData();
-  const { name, email, message, date } = loadDetails;
+  const { name, email, message, date, subject } = loadDetails;
+  const { user } = useAuth();
   return (
     <div className="w-[800px] mx-auto py-40">
       <a
         href="#"
         className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
-        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white my-8">
-          {name}
-        </h5>
+        <div className="flex justify-left items-center gap-5">
+          <div className="">
+            {user ? (
+              <img
+                className="w-8 h-8 rounded-full"
+                src={user?.photoURL}
+                alt="user photo"
+              />
+            ) : (
+              <FaUserCircle className="text-[#ed7966] text-lg md:text-3xl"></FaUserCircle>
+            )}
+          </div>
+          <h5 className=" text-xl font-bold tracking-tight text-gray-900 dark:text-white my-8">
+            {name}
+          </h5>
+        </div>
         <h5 className=" text-base tracking-tight text-gray-500 dark:text-white">
           {email}
         </h5>
@@ -19,7 +35,7 @@ const InboxDetailsPage = () => {
           Date: {date}
         </h5>
         <h5 className=" text-base font-bold tracking-tight text-gray-500 dark:text-white mb-8">
-          Subject:
+          Subject: {subject}
         </h5>
         <p className="font-normal text-gray-700 dark:text-gray-400 my-12">
           {message}
--- src/Pages/DashboardPages/AdminInbox/UserDataRow.jsx
+++ src/Pages/DashboardPages/AdminInbox/UserDataRow.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import axiosSecure from "../../../Api";
 
-const UserDataRow = ({ user, refetch, serialNo }) => {
-  const handleDelete = (user) => {
+const UserDataRow = ({ inboxUser, refetch, serialNo }) => {
+  const handleDelete = (inboxUser) => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -15,7 +15,7 @@ const UserDataRow = ({ user, refetch, serialNo }) => {
       confirmButtonText: "Yes, delete it!",
     }).then(async (result) => {
       if (result.isConfirmed) {
-        const res = await axiosSecure.delete(`/inbox/${user._id}`);
+        const res = await axiosSecure.delete(`/inbox/${inboxUser._id}`);
         console.log(res.data);
         if (res.data.deletedCount > 0) {
           // refetch to update the ui
@@ -23,7 +23,7 @@ const UserDataRow = ({ user, refetch, serialNo }) => {
           Swal.fire({
             position: "top-end",
             icon: "success",
-            title: `${user.name} has been deleted`,
+            title: `${inboxUser.name} has been deleted`,
             showConfirmButton: false,
             timer: 1500,
           });
@@ -37,21 +37,21 @@ const UserDataRow = ({ user, refetch, serialNo }) => {
         <td className="whitespace-nowrap px-6 py-6 font-semibold">
           {serialNo}
         </td>
-        <td className="whitespace-nowrap px-6 py-6">{user.name}</td>
-        <Link to={`/admin-dashboard/inboxDetails/${user._id}`}>
+        <td className="whitespace-nowrap px-6 py-6">{inboxUser.name}</td>
+        <Link to={`/admin-dashboard/inboxDetails/${inboxUser._id}`}>
           <td className="whitespace-nowrap px-6 py-8">
-            {user.message.split(" ").slice(0, 10).join(" ")}
+            {inboxUser.message.split(" ").slice(0, 10).join(" ")}
           </td>
         </Link>
         <td className="whitespace-nowrap px-6 py-6">
-          <button onClick={() => handleDelete(user)}>
+          <button onClick={() => handleDelete(inboxUser)}>
             <MdDelete
               title="Delete Message"
               className="text-red-600 text-3xl"
             />
           </button>
         </td>
-        <td className="whitespace-nowrap pr-6 py-6">{user.date}</td>
+        <td className="whitespace-nowrap pr-6 py-6">{inboxUser.date}</td>
       </tr>
     </>
   );
