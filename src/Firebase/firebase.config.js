d7f79053316f92f8ac136456e277f1d16eaf00b7
Nafis <bhuiyannafis517@gmail.com>
2024-02-23 15:11:57 +0600

    swap

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,18 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: import.meta.env.VITE_apiKey,
-  // authDomain: import.meta.env.VITE_authDomain,
-  // projectId: import.meta.env.VITE_projectId,
-  // storageBucket: import.meta.env.VITE_storageBucket,
-  // messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  // appId: import.meta.env.VITE_appId,
-  apiKey: "AIzaSyBkdQ_tB4PQGM4xJtBKulpGu_yUy_lHozA",
-  authDomain: "delta-translator-ac8d6.firebaseapp.com",
-  projectId: "delta-translator-ac8d6",
-  storageBucket: "delta-translator-ac8d6.appspot.com",
-  messagingSenderId: "380358348033",
-  appId: "1:380358348033:web:84d1c7cf83b00c81159f00"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+
 };
 
 // Initialize Firebase
