8920140a1d124bc980982515622a546c68aa4867
deltatranslator <deltatranslator16@gmail.com>
2024-02-27 01:28:59 +0600

    bug fixed

--- src/components/UserDashboard/Modals/ContactInfo.jsx
+++ src/components/UserDashboard/Modals/ContactInfo.jsx
@@ -3,11 +3,11 @@ import { GiCrossMark } from "react-icons/gi";
 import { FaChevronRight } from "react-icons/fa";
 import useUser from "../../../hooks/useUser";
 import useAuth from "../../../hooks/useAuth";
-import {ReactiveButton} from "reactive-button";
+import ReactiveButton from "reactive-button";
 import { useState } from "react";
-import axiosSecure from "../../../api";
 import useProfile from "../../../hooks/useProfile";
 import toast from "react-hot-toast";
+import axiosSecure from "../../../Api";
 
 const ContactInfo = () => {
   const [state, setState] = useState("idle");
@@ -116,7 +116,6 @@ const ContactInfo = () => {
                     />
                   </div>
                   <div className=" flex items-center justify-end mt-2">
-                  
                     <ReactiveButton
                       buttonState={state}
                       idleText="Submit"
