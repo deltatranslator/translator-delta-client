f527841b58641a97149de1b266fa8444441c7493
unknown <morzmamun@gmail.com>
2024-03-01 01:35:25 +0600

    polishing FAQ section

--- src/components/FAQSection/FAQSection.jsx
+++ src/components/FAQSection/FAQSection.jsx
@@ -26,7 +26,7 @@ const FAQSection = () => {
   return (
     <div className="flex md:space-y-6 flex-col shadow-md rounded-md container mx-auto bg-gray-100 py-10 dark:bg-slate-800">
       <div className="md:w-1/2 mx-auto space-y-4 text-center">
-        <h1 className="text-[#303179] font-bold text-2xl">
+        <h1 className="text-[#303179] font-bold text-2xl dark:text-[#ed7966]">
           Why Choose Delta Translator!
         </h1>
         <p className="text-ellipsis">
@@ -38,35 +38,35 @@ const FAQSection = () => {
       </div>
       <div className="flex flex-col px-1 py-5 md:px-10 md:py-5 lg:px-20  lg:flex-row lg:gap-1 ">
         <div className="flex-1 py-5 rounded-xl md:space-y-8">
-          <h1 className="text-ellipsis font-medium text-[#303179] text-xl lg:text-2xl mb-5">
+          <h1 className="text-ellipsis font-medium text-[#303179] text-xl lg:text-2xl mb-5 dark:text-[#ed7966]">
             What you can do with Delta Translator.
           </h1>
 
           {/* Delta translator benefit */}
           <div className="space-y-4">
             <div className="flex items-center gap-2">
-              <IoCheckmarkDoneSharp className="size-5 text-[#ed7966]"></IoCheckmarkDoneSharp>
+              <IoCheckmarkDoneSharp className="size-5 dark:text-[#ed7966] text-[#303179] "></IoCheckmarkDoneSharp>
               <p>Translate longer texts</p>
             </div>
 
             <div className="flex items-center  gap-2">
-              <IoCheckmarkDoneSharp className="size-5 text-[#ed7966]"></IoCheckmarkDoneSharp>
+              <IoCheckmarkDoneSharp className="size-5 dark:text-[#ed7966] text-[#303179]"></IoCheckmarkDoneSharp>
               <p>Use a translator without ads</p>
             </div>
             <div className="flex items-center  gap-2">
-              <IoCheckmarkDoneSharp className="size-5 text-[#ed7966]"></IoCheckmarkDoneSharp>
+              <IoCheckmarkDoneSharp className="size-5 dark:text-[#ed7966] text-[#303179]"></IoCheckmarkDoneSharp>
               <p>Translate text in 93 languages</p>
             </div>
             <div className="flex items-center  gap-2">
-              <IoCheckmarkDoneSharp className="size-5 text-[#ed7966]"></IoCheckmarkDoneSharp>
+              <IoCheckmarkDoneSharp className="size-5 dark:text-[#ed7966] text-[#303179]"></IoCheckmarkDoneSharp>
               <p>Enjoy completely free translation</p>
             </div>
             <div className="flex items-center  gap-2">
-              <IoCheckmarkDoneSharp className="size-5 text-[#ed7966]"></IoCheckmarkDoneSharp>
+              <IoCheckmarkDoneSharp className="size-5 dark:text-[#ed7966] text-[#303179]"></IoCheckmarkDoneSharp>
               <p>Translate online without downloading an app</p>
             </div>
             <div className="flex items-center  gap-2">
-              <IoCheckmarkDoneSharp className="size-5 text-[#ed7966]"></IoCheckmarkDoneSharp>
+              <IoCheckmarkDoneSharp className="size-5 dark:text-[#ed7966] text-[#303179]"></IoCheckmarkDoneSharp>
               <p>Translate between languages on a mobile-friendly platform</p>
             </div>
           </div>
@@ -78,7 +78,7 @@ const FAQSection = () => {
             FAQs
           </h1>
           <div className="space-y-3">
-            <div className="px-2 py-4 shadow-md rounded-md  shadow-[#303179]  dark:shadow-white">
+            <div className="px-2 py-4 shadow-sm rounded-md  shadow-[#303179]  dark:shadow-white">
               <div className="space-y-2">
                 <div className="flex justify-between items-center">
                   <p> Can I use Delta Translator for Free?</p>
@@ -99,7 +99,7 @@ const FAQSection = () => {
               </div>
             </div>
 
-            <div className="px-2 py-4 shadow-md dark:shadow-white shadow-[#303179] rounded-md">
+            <div className="px-2 py-4 shadow-sm dark:shadow-white shadow-[#303179] rounded-md">
               <div className="space-y-2">
                 <div className="flex justify-between items-center">
                   <p> How many Languages does Delta Translator Support?</p>
@@ -112,14 +112,14 @@ const FAQSection = () => {
                   </div>
                 </div>
                 {questionOpen2 && (
-                  <p className="text-[#303179] font-medium">
+                  <p className="text-[#303179] font-medium dark:text-[#ed7966]">
                     Currently, It lets you translate text into 93 languages,
                     with more to come.
                   </p>
                 )}
               </div>
             </div>
-            <div className="px-2 py-4 shadow-md shadow-[#303179]  dark:shadow-white rounded-md">
+            <div className="px-2 py-4 shadow-sm shadow-[#303179]  dark:shadow-white rounded-md">
               <div className="space-y-2">
                 <div className="flex justify-between items-center">
                   <p>How many characters can translate at once?</p>
@@ -132,7 +132,7 @@ const FAQSection = () => {
                   </div>
                 </div>
                 {questionOpen3 && (
-                  <p className="text-[#303179] font-medium">
+                  <p className="text-[#303179] font-medium dark:text-[#ed7966]">
                     Delta Translator let you translate 500 characters at a time,
                     but you can always delete a text and type in something else.
                     Stay tuned - soon you will be able to upload an entire pdf
@@ -141,7 +141,7 @@ const FAQSection = () => {
                 )}
               </div>
             </div>
-            <div className="px-2 py-4 shadow-md shadow-[#303179]  dark:shadow-white rounded-md">
+            <div className="px-2 py-4 shadow-sm shadow-[#303179]  dark:shadow-white rounded-md">
               <div className="space-y-2">
                 <div className="flex justify-between items-center">
                   <p>How accurate is this translator?</p>
@@ -153,7 +153,7 @@ const FAQSection = () => {
                     )}
                   </div>
                 </div>
-                {questionOpen4 && <p>yes</p>}
+                {questionOpen4 && <p className="text-[#303179] font-medium dark:text-[#ed7966]">yes</p>}
               </div>
             </div>
           </div>
