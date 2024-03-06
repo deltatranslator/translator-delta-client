1cfacb44a74ca946bdf1a2914cb9fa8a7e29d12c
deltatranslator <deltatranslator16@gmail.com>
2024-03-06 15:22:13 +0600

    latest commit

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -12,7 +12,8 @@ import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 import axiosSecure from "../../api";
-import useUser from "../../hooks/useUser";
+import { MdOutlineMailLock } from "react-icons/md";
+import { IoIosPerson } from "react-icons/io";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
@@ -20,7 +21,6 @@ const SignUp = () => {
   const { createUser, userProfileUpdate } = useAuth();
   const navigate = useNavigate();
   const [imgTitle, setImgTitle] = useState("");
-  const { refetch } = useUser();
 
   const {
     register,
@@ -73,7 +73,6 @@ const SignUp = () => {
           toast.success("Successfully signed up");
           reset();
           navigate("/");
-          refetch();
         });
       });
     } catch (err) {
@@ -84,9 +83,7 @@ const SignUp = () => {
   // console.log(imgTitle);
   return (
     <div className="hero  sign-back min-h-screen  dark:bg-black ">
-
       <div className="hero-content flex flex-col md:flex-row-reverse w-full lg:gap-10">
-
         <div className="card form-data flex-shrink-0 w-80 md:w-96 lg:w-[450px] py-7 bg-[#ffffff] bg-opacity-10 backdrop-blur-sm shadow-black shadow-2xl">
           <div className="text-left ml-10 text-[#213d5e] text-2xl md:text-4xl font-bold">
             Create Your Account
@@ -130,8 +127,7 @@ const SignUp = () => {
               )}
             </div>
 
-            <div >
-
+            <div>
               <div className="flex flex-col ">
                 <div className="form-control flex flex-row my-3">
                   <div className="px-3 py-2 rounded-l-lg bg-[#213d5e]  shadow-[#213d5e] shadow-lg">
@@ -152,41 +148,17 @@ const SignUp = () => {
                       className="input rounded-l-none w-full input-bordered border-[#213d5e] shadow-[#213d5e] shadow-lg"
                     />
                     {/* Text changes based on visibility */}
-                    <button onClick={togglePasswordVisibility} className="relative -ml-7 md:-ml-10 ">
-                      {showPassword ? <FaEyeSlash className="text-[#213d5e]" size={20} /> : <FaEye className="text-[#213d5e]" size={20} />}
+                    <button
+                      onClick={togglePasswordVisibility}
+                      className="relative -ml-7 md:-ml-10 "
+                    >
+                      {showPassword ? (
+                        <FaEyeSlash className="text-[#213d5e]" size={20} />
+                      ) : (
+                        <FaEye className="text-[#213d5e]" size={20} />
+                      )}
                     </button>
                   </div>
-
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-[#ed7966] font-semibold">
-                    Password
-                  </span>
-                </label>
-                <div className="flex flex-row items-center ">
-                  <input
-                    {...register("password", {
-                      required: true,
-                      minLength: 8,
-                      maxLength: 20,
-                      pattern: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/,
-                    })}
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    placeholder="  Password"
-                    className="input w-full input-bordered border-[#ed7966]"
-                  />
-                  {/* Text changes based on visibility */}
-                  <button
-                    onClick={togglePasswordVisibility}
-                    className="relative -ml-7 md:-ml-10 "
-                  >
-                    {showPassword ? (
-                      <FaEyeSlash className="text-[#ed7966]" size={20} />
-                    ) : (
-                      <FaEye className="text-[#ed7966]" size={20} />
-                    )}
-                  </button>
                 </div>
 
                 <div className="text-center">
@@ -196,7 +168,10 @@ const SignUp = () => {
                     </span>
                   )}
                   {errors.password?.type === "minLength" && (
-                    <p className="text-red-600"> Password must be 8 character</p>
+                    <p className="text-red-600">
+                      {" "}
+                      Password must be 8 character
+                    </p>
                   )}
                   {errors.password?.type === "maxLength" && (
                     <p className="text-red-600">
@@ -213,14 +188,9 @@ const SignUp = () => {
                   )}
                 </div>
               </div>
-
-
-
             </div>
             <div>
               <div className="my-2 flex items-center justify-center">
-
-
                 <div className="px-3 py-[11px] rounded-l-lg bg-[#213d5e]  shadow-[#213d5e] shadow-lg -mt-[5px]">
                   <GoUpload className="text-2xl text-white" />
                 </div>
@@ -228,15 +198,11 @@ const SignUp = () => {
                   htmlFor="image"
                   className="file-label shadow-[#213d5e] shadow-lg mb-2 text-sm text-white rounded-xl "
                 >
-
-
-                  <div >
+                  <div>
                     {imgTitle ? (
                       <p>{imgTitle.slice(0, 20)}</p>
                     ) : (
                       <div className="flex justify-center items-center mx-auto">
-
-
                         <p>Upload Profile</p>
                       </div>
                     )}
@@ -253,7 +219,29 @@ const SignUp = () => {
                 onChange={(e) => setImgTitle(e.target.files[0].name)}
               />
             </div>
-            {/* social login  */}
+            {errors.image && (
+              <span className="text-red-700 font-bold">image is required</span>
+            )}
+            <div className="form-control mt-6">
+              <button
+                type="submit"
+                className="btn border-0 bg-[#213d5e] shadow-[#243243] shadow-lg hover:bg-[#00ABE4] text-white font-semibold"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <p className="text-center -mt-4">
+            <small className="text-neutral-700 ">
+              Already have an account?{" "}
+              <Link to="/login">
+                <span className="font-extrabold ">Login</span>
+              </Link>
+            </small>
+          </p>
+          <div className="ml-10 social-login">
+            <SocialLogin />
           </div>
           {/* social login  */}
           <div className="flex justify-center home-btn ">
@@ -262,16 +250,15 @@ const SignUp = () => {
               to="/"
             >
               <p>Go to</p>
-              <span className="underline font-extrabold text-base text-[#213d5e]">Home</span>
+              <span className="underline font-extrabold text-base text-[#213d5e]">
+                Home
+              </span>
             </Link>
           </div>
-          <div>
-          </div>
-
+          <div></div>
         </div>
       </div>
     </div>
-
   );
 };
 
