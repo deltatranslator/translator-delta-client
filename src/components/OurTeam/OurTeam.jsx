18f87a327df086ba9da19b181e2f134cfb0d0eab
Nafis <bhuiyannafis517@gmail.com>
2024-03-06 17:44:27 +0600

    layout done

--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -4,13 +4,13 @@ import TeamMembers from "./TeamMembers";
 const OurTeam = () => {
     return (
         <div>
-            <div className="md:flex justify-around">
-               <div className="bg-[#303179] h-80 lg:w-1/4 rounded-tr-3xl rounded-br-full border-e-4">
+            <div className="md:flex justify-around lg:ml-4">
+               <div className="bg-[#303179] h-80 lg:w-1/3 rounded-tr-3xl rounded-br-full border-e-4">
                 <p className="text-[#00ABE4] text-2xl font-bold border-blue-400 border-b-2 mx-3 my-3 text-center ">Delta Translate</p>
                 <p className="text-white my-8 text-center font-bold text-3xl">Our Team Section <br />for informing the <br />clients at all .</p>
 
                </div>
-               <div className="">
+               <div className="my-5 ">
                 <div className="flex gap-8 md:gap-16 ml-5">
                 <img className="rounded-full w-24 mx-auto ml-6" src="https://i.ibb.co/Q6dKK4N/1709542185354.jpg" />
                <img className="rounded-full mx-auto w-24 " src="https://i.ibb.co/0n4j7Vz/1698063591767.jpg" />
